52f825f21b79f9e95ff913c263120e7c5a1e6e00
ewbyf <107148516+ewbyf@users.noreply.github.com>
2023-02-06 23:46:22 -0600

    faq updated

--- src/components/common/FAQ/FAQSegment.tsx
+++ src/components/common/FAQ/FAQSegment.tsx
@@ -3,67 +3,47 @@ import styled, { AnyStyledComponent } from "styled-components";
 
 import Answer from "./Answer";
 
+import {
+  trees,
+  ocean,
+  mountains,
+  pond,
+  desert,
+  flowers,
+} from "../../../images/Images";
+
 interface ISegmentProps {
   title: string;
+  image: string;
   children: string;
-  titleColor?: string;
 }
-const FlexCenter: AnyStyledComponent = styled.div`
-  justify-content: center;
+
+const FlexRow: AnyStyledComponent = styled.div`
   display: flex;
   width: 100%;
-  padding: 0;
-`;
-const FlexColumn: AnyStyledComponent = styled.div`
-  display: flex;
-
-  flex-direction: column;
-`;
-const Group: AnyStyledComponent = styled.div`
-  padding: 10px;
-  padding-top: 0px;
-  background-color: white;
-  border-radius: 15px;
-  margin-bottom: 10px;
-  margin: 5px;
+  margin-top: 62px;
 `;
 
 const Title: AnyStyledComponent = styled.div`
-  font-family: "Amiko-Bold", cursive;
-  font-size: 150%;
-  padding: 3%;
-  color: ${(props: ISegmentProps) => props.titleColor};
-  padding-top: 5%;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 1em;
-  letter-spacing: 1px;
+  font-family: "MerriweatherSans-Regular";
+  font-size: 64px;
   @media (max-width: 600px) {
     font-size: 5vw;
   }
+  color: white;
 `;
-export default ({ title, children, titleColor }: ISegmentProps) => {
-  function titlePicker() {
-    switch (title) {
-      default:
-        return "#000000";
-    }
-  }
 
+const Image: AnyStyledComponent = styled.img`
+  width: 300px;
+  height: 279.4px;
+  margin-right: 33px;
+`;
+
+export default ({ title, image, children }: ISegmentProps) => {
   return (
-    <div>
-      <Group>
-        <FlexColumn>
-          <FlexCenter>
-            <Title titleColor={titlePicker()}>
-              <span style={{ color: "#f78702" }}>{titleColor}</span> {title}
-            </Title>
-          </FlexCenter>
-          <FlexCenter>
-            <Answer>{children}</Answer>
-          </FlexCenter>
-        </FlexColumn>
-      </Group>
-    </div>
+    <FlexRow>
+      <Image src={image}/>
+      <Title>{title}</Title>
+    </FlexRow>
   );
 };
--- src/components/sections/FAQ.tsx
+++ src/components/sections/FAQ.tsx
@@ -2,97 +2,81 @@ import React from "react";
 import styled, { AnyStyledComponent } from "styled-components";
 import FAQSegment from "../common/FAQ/FAQSegment";
 
+import {
+  trees,
+  ocean,
+  mountains,
+  pond,
+  desert,
+  flowers,
+} from "../../images/Images";
+
 const Title: AnyStyledComponent = styled.div`
-  font-size: 4.5vw;
-  color: #009ac7;
-  font-family: "ZenDots-Regular", cursive;
-  text-shadow: 2px 2px 4px gray;
-  @media screen and (max-width: 1000px) {
-    font-size: 8vw;
-  }
+  font-size: 96px;
+  color: white;
+  font-family: "Alike-Regular";
+  padding-left: 27px;
+  border-left: 10px #148648 solid;
 `;
 const FlexCenter: AnyStyledComponent = styled.div`
-  justify-content: center;
   display: flex;
-
+  justify-content: center;
   padding: 0;
 `;
 const FlexColumn: AnyStyledComponent = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
+  margin-left: 67px;
 `;
+const FlexRow: AnyStyledComponent = styled.div`
+  display: flex;
+`;
+
 const Constraint: AnyStyledComponent = styled.div`
   display: flex;
-
   max-width: 600px;
 `;
 export default (props: any) => {
   return (
-    <div>
       <FlexColumn>
-        <FlexCenter>
-          <Title>FAQ</Title>
-        </FlexCenter>
-        <FlexCenter>
-          <Constraint>
-            <FlexCenter>
-              <FlexColumn>
-                <FAQSegment titleColor="WHAT " title="is a hackathon?">
-                  A hackathon is a weekend long event for anyone interested in
-                  tech to build, learn, and grow! It features many opportunities
-                  to explore your technical skills and interact with others who
-                  are just as passionate and curious about how tech impacts the
-                  world around us.
-                </FAQSegment>
-                <FAQSegment
-                  titleColor="WHERE"
-                  title="is PickHacks 2022 going to be?"
-                >
-                  With the world still in limbo over COVID, PickHacks 2022 will
-                  be held from the best coding environment in the world, your
-                  room! This year’s iteration will be virtual, with activities
-                  and events planned accordingly.
-                </FAQSegment>
-                <FAQSegment titleColor="WHEN" title="is PickHacks going on?">
-                  PickHacks 2022 will be from 4:00 pm, April 8th to 3:00 pm,
-                  April 10th.
-                </FAQSegment>
-                <FAQSegment titleColor="WHO" title="can participate?">
-                  PickHacks is open to any and all college or high school
-                  students. Just grab a team of up to 3 other hackers and start
-                  building!
-                </FAQSegment>
-                <FAQSegment
-                  titleColor="WHAT"
-                  title="if I can’t think of a hack?"
-                >
-                  No worries! Try to think of some problem that exists in the
-                  realm of our theme. If that leads nowhere, try consulting some
-                  of our resources or experts for inspo.
-                </FAQSegment>
+        <Title>FAQ</Title>
+          <FAQSegment title="WHO can participate?" image={flowers}>
+            A hackathon is a weekend long event for anyone interested in
+            tech to build, learn, and grow! It features many opportunities
+            to explore your technical skills and interact with others who
+            are just as passionate and curious about how tech impacts the
+            world around us.
+          </FAQSegment>
+          <FAQSegment title="WHAT is a hackathon?" image={desert}>
+            With the world still in limbo over COVID, PickHacks 2022 will
+            be held from the best coding environment in the world, your
+            room! This year’s iteration will be virtual, with activities
+            and events planned accordingly.
+          </FAQSegment>
+          <FAQSegment title="WHERE is PickHacks located?" image={pond}>
+            PickHacks 2022 will be from 4:00 pm, April 8th to 3:00 pm,
+            April 10th.
+          </FAQSegment>
+          <FAQSegment title="WHEN is PickHacks?" image={mountains}>
+            PickHacks is open to any and all college or high school
+            students. Just grab a team of up to 3 other hackers and start
+            building!
+          </FAQSegment>
+          <FAQSegment title="WHY should I participate?" image={ocean}>
+            No worries! Try to think of some problem that exists in the
+            realm of our theme. If that leads nowhere, try consulting some
+            of our resources or experts for inspo.
+          </FAQSegment>
 
-                <FAQSegment
-                  titleColor="WHEN"
-                  title="will I get my swag?"
-                >
-                  Due to shipping delays from COVID, you can expect to receive 
-                  your swag sometime around late April. However, we don't want 
-                  to keep you in suspense! You can expect to recieve a sweatshirt, 
-                  stickers, and more awesome goodies! We will not be shipping swag 
-                  to hackers outside of the US and Canada.
-                </FAQSegment>
+          <FAQSegment title="HOW do I apply?" image={trees}>
+            Due to shipping delays from COVID, you can expect to receive 
+            your swag sometime around late April. However, we don't want 
+            to keep you in suspense! You can expect to recieve a sweatshirt, 
+            stickers, and more awesome goodies! We will not be shipping swag 
+            to hackers outside of the US and Canada.
+          </FAQSegment>
 
-                <FAQSegment titleColor="MORE" title="questions?">
-                  Contact any of our team members on our Discord, email us at
-                  msthackathon@umsystem.edu, or DM us on instagram
-                  @sandtpickhacks.
-                </FAQSegment>
-              </FlexColumn>
-            </FlexCenter>
-          </Constraint>
-        </FlexCenter>
       </FlexColumn>
-    </div>
   );
 };
